1dae3a66a213f91d00863ab12e5534607f3ec944
106519 <aravind.gopalan@scientificgames.com>
2017-08-03 18:58:29 +0530

    Adding CRUD opetions in repository

--- sgprojectmanagement/Models/EmployeeProfile.cs
+++ sgprojectmanagement/Models/EmployeeProfile.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace sgprojectmanagement.Models
 {
     public class EmployeeProfile
     {
+        [BsonElement("_id")]
         public string EmployeeId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
--- sgprojectmanagement/Repository/EmployeeProfileRepository.cs
+++ sgprojectmanagement/Repository/EmployeeProfileRepository.cs
@@ -1,11 +1,67 @@
-﻿using System;
+﻿using MongoDB.Driver;
+using MongoDB.Bson;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using sgprojectmanagement.Models;
 
 namespace sgprojectmanagement.Repository
 {
     public class EmployeeProfileRepository
     {
+
+        private IMongoDatabase _database;
+        private string _tableName;
+        private IMongoCollection<EmployeeProfile> _collection;
+        //private MongoCollectionSettings settings;
+        public EmployeeProfileRepository(MongoDatabaseBase db, string tblName)
+        {
+            _database = db;
+            _tableName = tblName;
+            _collection = _database.GetCollection<EmployeeProfile>(_tableName);
+            //_database.GetCollection<T>(tblName);
+        }
+        public async Task<EmployeeProfile> Get(string id)
+        {
+            var filter = Builders<EmployeeProfile>.Filter.Eq("EmployeeId", id);
+            return await _collection.Find<EmployeeProfile>(filter).FirstOrDefaultAsync();
+        }
+
+        ///<summary>  
+        /// Get all records   
+        ///</summary>  
+        ///<returns></returns>  
+        public async Task<IEnumerable<EmployeeProfile>> GetAll()
+        {
+            return await _collection.Find(_ => true).ToListAsync();
+        }
+
+        ///<summary>  
+        /// Generic add method to insert enities to collection   
+        ///</summary>  
+        ///<param name="entity"></param>  
+        public async void Add(EmployeeProfile entity)
+        {
+            await _collection.InsertOneAsync(entity);
+        }
+        ///<summary>  
+        /// Generic delete method to delete record on the basis of id  
+        ///</summary>  
+        ///<param name="id"></param>  
+        public async void Delete(string id)
+        {
+            var filter = Builders<EmployeeProfile>.Filter.Eq("EmployeeId", id);
+            await _collection.DeleteOneAsync(filter);
+        }
+
+        public async void Update(string id,string firstName)
+        {
+            var filter = Builders<EmployeeProfile>.Filter.Eq(s => s.EmployeeId, id);
+            var update = Builders<EmployeeProfile>.Update
+                                .Set(s => s.FirstName, firstName);
+             await _collection.UpdateOneAsync(filter, update);
+        }
     }
 }
